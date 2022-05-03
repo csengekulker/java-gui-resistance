54292b4ab0997bf02e4a1d76febefb494107f95d
csengekulker <k71608883429@ktch.hu>
2022-05-03 15:42:02 +0200

    set panels

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,15 +1,60 @@
-public class MainFrame {
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame{
+
+  JPanel mainPanel;
+  JPanel northPanel;
+  JPanel southPanel;
   
   public MainFrame() {
-
+    setFrame();
+    setPanels();
+    setComponents();
   }
 
   private void setFrame() {
-  
+    this.setTitle("Eredő ellenállások");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(500, 500);
+    this.setVisible(true);
   }
 
   private void setPanels() {
-      
+    this.mainPanel = new JPanel();
+    this.northPanel = new JPanel();
+    this.southPanel = new JPanel();
+
+    mainPanel.setBackground(Color.DARK_GRAY);
+    northPanel.setBackground(Color.GRAY);
+    mainPanel.setBackground(Color.GRAY);
+
+
+    // layout settings for window
+    this.setLayout(
+      new GridLayout(1, 1)
+    );
+
+    // layout settings for mainpanel
+    mainPanel.setLayout(
+      new BorderLayout()
+    );
+
+    // panelsize settings
+
+    mainPanel.setSize(500, 420);
+    northPanel.setSize(500, 40);
+    southPanel.setSize(500, 40);
+
+    //attach panels to main
+    mainPanel.add(northPanel, BorderLayout.NORTH);
+    mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+    this.add(mainPanel);
+    
   }
 
   private void setComponents() {
