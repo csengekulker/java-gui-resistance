d6d9b3c6eae02e0eac253916a883089811f26cf5
csengekulker <k71608883429@ktch.hu>
2022-05-03 15:32:33 +0200

    initial commit

---
+++ src/MainFrame.java
@@ -0,0 +1,19 @@
+public class MainFrame {
+  
+  public MainFrame() {
+
+  }
+
+  private void setFrame() {
+  
+  }
+
+  private void setPanels() {
+      
+  }
+
+  private void setComponents() {
+
+  }
+
+}
